6b123645ea3b8c0c7a4c3b6d8cafb455bd0d4c3d
EC2 Default User <ec2-user@ip-172-31-21-68.ap-northeast-1.compute.internal>
2022-10-22 05:47:10 +0000

    [Update]応用課題2-10

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :authenticate_user!,except: [:top, :about]
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
   def after_sign_in_path_for(resource)
-    root_path
+    user_path(resource)
   end
 
   def after_sign_out_path_for(resource)
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  belongs_to :books
+  has_many :books
   has_one_attached :profile_image
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
