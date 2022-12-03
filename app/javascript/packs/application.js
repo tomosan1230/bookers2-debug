40347e6eae2f3d4026c846d9abfce304213564e6
EC2 Default User <ec2-user@ip-172-31-21-68.ap-northeast-1.compute.internal>
2022-12-03 12:12:01 +0000

    [Start]応用課題7d

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -19,3 +19,10 @@ import "../stylesheets/application";
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+import Raty from "raty.js"
+window.raty = function(elem,opt) {
+  let raty =  new Raty(elem,opt)
+  raty.init();
+  return raty;
+}
